c62db19abf2d33b390ab39d5d0c7c0c75f23945b
hhc <2811895673@qq.com>
2026-04-22 21:53:35 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -44,6 +44,28 @@ impl Default for Person {
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+           return Person::default();
+        }
+        let parts: Vec<&str> = s.split(',').collect();
+
+        if parts.len() != 2 {
+            return Person::default();
+        }
+        let name = parts[0].trim(); // 提取名字
+        let age_str = parts[1].trim(); // 提取年龄字符串
+
+        if name.is_empty() {
+            return Person::default();
+        }
+
+        match age_str.parse::<usize>() {
+            Ok(age) => Person {
+                name: name.to_string(),
+                age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
